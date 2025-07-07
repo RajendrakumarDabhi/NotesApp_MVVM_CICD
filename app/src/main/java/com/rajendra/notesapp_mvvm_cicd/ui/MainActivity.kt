f28bae7983df2f387dd0d4c923717fb06e478e93
Rajendrakumar <rajedradabhi79@gmail.com>
2025-07-07 19:07:48 +0530

    add MainViewModel for theme management and update MainActivity to use it

--- app/src/main/java/com/rajendra/notesapp_mvvm_cicd/ui/MainActivity.kt
+++ app/src/main/java/com/rajendra/notesapp_mvvm_cicd/ui/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.rajendra.notesapp_mvvm_cicd.ui
 
+import android.R.attr.navigationIcon
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ColorLens
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
@@ -23,13 +34,34 @@ class MainActivity : ComponentActivity() {
 
     val viewmmodel: MainViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppTheme(darkTheme = viewmmodel.isDark.value) {
-                Surface(modifier = Modifier) {
-                    NotesNavHost()
-                }
+                Scaffold(
+                    modifier = Modifier,
+                    topBar = {
+                        TopAppBar(
+                            // You can set a navigation icon if needed
+                            actions = {
+                                Image(
+                                    imageVector = Icons.Default.ColorLens,
+                                    contentDescription = "Back",
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .clickable {
+                                            viewmmodel.toggleTheme()
+                                        }
+                                )
+                            },
+                            title = { Text("Notes") },
+                        )
+                    },
+                    content = {
+                        NotesNavHost()
+                    })
             }
         }
     }
